ecfac7563b9482c7f220014fa48c8a27f2bc2582
hidari-nut <madevis02@gmail.com>
2023-05-01 15:35:28 +0700

    sqlparam

--- SORA_Project/SORA_Project/FormMenu.Designer.cs
+++ SORA_Project/SORA_Project/FormMenu.Designer.cs
@@ -28,24 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-
             this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
-            this.pictureBoxWave = new System.Windows.Forms.PictureBox();
             this.buttonAccount = new System.Windows.Forms.Button();
             this.buttonTransfer = new System.Windows.Forms.Button();
             this.buttonMerchant = new System.Windows.Forms.Button();
             this.buttonHistory = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWave)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxLogo
             // 
             this.pictureBoxLogo.BackgroundImage = global::SORA_Project.Properties.Resources.Logo_2;
             this.pictureBoxLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBoxLogo.Location = new System.Drawing.Point(157, 10);
+            this.pictureBoxLogo.Location = new System.Drawing.Point(179, 13);
+            this.pictureBoxLogo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBoxLogo.Name = "pictureBoxLogo";
-            this.pictureBoxLogo.Size = new System.Drawing.Size(492, 185);
+            this.pictureBoxLogo.Size = new System.Drawing.Size(562, 247);
             this.pictureBoxLogo.TabIndex = 1;
             this.pictureBoxLogo.TabStop = false;
             // 
@@ -56,10 +54,9 @@
             this.buttonAccount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.buttonAccount.ForeColor = System.Drawing.Color.White;
-            this.buttonAccount.Location = new System.Drawing.Point(33, 243);
-            this.buttonAccount.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonAccount.Location = new System.Drawing.Point(38, 324);
             this.buttonAccount.Name = "buttonAccount";
-            this.buttonAccount.Size = new System.Drawing.Size(152, 81);
+            this.buttonAccount.Size = new System.Drawing.Size(174, 108);
             this.buttonAccount.TabIndex = 4;
             this.buttonAccount.UseVisualStyleBackColor = true;
             // 
@@ -70,10 +67,9 @@
             this.buttonTransfer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonTransfer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.buttonTransfer.ForeColor = System.Drawing.Color.White;
-            this.buttonTransfer.Location = new System.Drawing.Point(224, 243);
-            this.buttonTransfer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonTransfer.Location = new System.Drawing.Point(256, 324);
             this.buttonTransfer.Name = "buttonTransfer";
-            this.buttonTransfer.Size = new System.Drawing.Size(152, 81);
+            this.buttonTransfer.Size = new System.Drawing.Size(174, 108);
             this.buttonTransfer.TabIndex = 5;
             this.buttonTransfer.UseVisualStyleBackColor = true;
             // 
@@ -84,10 +80,9 @@
             this.buttonMerchant.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonMerchant.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.buttonMerchant.ForeColor = System.Drawing.Color.White;
-            this.buttonMerchant.Location = new System.Drawing.Point(410, 243);
-            this.buttonMerchant.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonMerchant.Location = new System.Drawing.Point(469, 324);
             this.buttonMerchant.Name = "buttonMerchant";
-            this.buttonMerchant.Size = new System.Drawing.Size(152, 81);
+            this.buttonMerchant.Size = new System.Drawing.Size(174, 108);
             this.buttonMerchant.TabIndex = 6;
             this.buttonMerchant.UseVisualStyleBackColor = true;
             // 
@@ -98,35 +93,33 @@
             this.buttonHistory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.buttonHistory.ForeColor = System.Drawing.Color.White;
-            this.buttonHistory.Location = new System.Drawing.Point(601, 243);
-            this.buttonHistory.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonHistory.Location = new System.Drawing.Point(687, 324);
             this.buttonHistory.Name = "buttonHistory";
-            this.buttonHistory.Size = new System.Drawing.Size(152, 81);
+            this.buttonHistory.Size = new System.Drawing.Size(174, 108);
             this.buttonHistory.TabIndex = 7;
             this.buttonHistory.UseVisualStyleBackColor = true;
             // 
             // FormMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 600);
             this.Controls.Add(this.buttonHistory);
             this.Controls.Add(this.buttonMerchant);
             this.Controls.Add(this.buttonTransfer);
             this.Controls.Add(this.buttonAccount);
-            this.Controls.Add(this.pictureBoxWave);
             this.Controls.Add(this.pictureBoxLogo);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FormMenu";
             this.Text = "FormMenu";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWave)).EndInit();
             this.ResumeLayout(false);
+
         }
 
         #endregion
         private PictureBox pictureBoxLogo;
-        private PictureBox pictureBoxWave;
         private Button buttonAccount;
         private Button buttonTransfer;
         private Button buttonMerchant;
