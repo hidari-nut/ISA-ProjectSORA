039933e355fd43cbc6d32bd4735268334e35bbea
yuveex <alvinsetiawan2003@gmail.com>
2023-05-01 13:07:48 +0700

    Transaction properties

--- SORA_Project/SORA_Class/Transaksi.cs
+++ SORA_Project/SORA_Class/Transaksi.cs
@@ -13,5 +13,13 @@ namespace SORA_Class
         Customer recepient;
         DateTime transactionDate;
         double nominal;
+        bool completed;
+
+        public int Id { get => id; set => id = value; }
+        public Customer Sender { get => sender; set => sender = value; }
+        public Customer Recepient { get => recepient; set => recepient = value; }
+        public DateTime TransactionDate { get => transactionDate; set => transactionDate = value; }
+        public double Nominal { get => nominal; set => nominal = value; }
+        public bool Completed { get => completed; set => completed = value; }
     }
 }
