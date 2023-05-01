f9d31dedde45dad6b223229456275b1bddeeac39
yuveex <alvinsetiawan2003@gmail.com>
2023-05-01 16:06:00 +0700

    Transaction Read Data
    Added method to read unprocessed transactions to be processed in Customer.

--- SORA_Project/SORA_Class/Transaction.cs
+++ SORA_Project/SORA_Class/Transaction.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Principal;
 using System.Text;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 using System.Transactions;
 
@@ -17,15 +18,36 @@ namespace SORA_Class
         Customer sender;
         Customer recepient;
         DateTime transactionDate;
-        double nominal;
+        decimal nominal;
         bool completed;
 
         public int Id { get => id; set => id = value; }
         public Customer Sender { get => sender; set => sender = value; }
         public Customer Recepient { get => recepient; set => recepient = value; }
         public DateTime TransactionDate { get => transactionDate; set => transactionDate = value; }
-        public double Nominal { get => nominal; set => nominal = value; }
+        public decimal Nominal { get => nominal; set => nominal = value; }
         public bool Completed { get => completed; set => completed = value; }
+
+        public Transaction(int id, Customer sender, Customer recepient, DateTime transactionDate,
+            decimal nominal, bool completed)
+        {
+            Id = id;
+            Sender = sender;
+            Recepient = recepient;
+            TransactionDate = transactionDate;
+            Nominal = nominal;
+            Completed = completed;
+        }
+
+        public Transaction()
+        {
+            Id = 0;
+            Sender = new Customer();
+            Recepient = new Customer();
+            TransactionDate = DateTime.Now;
+            Nominal = 0;
+            Completed = true;
+        }
 
         public static bool Add(Transaction transaction)
         {
@@ -110,9 +132,13 @@ namespace SORA_Class
         /// </summary>
         /// <param name="customerId">Customer's ID</param>
         /// <returns>A list of unprocessed incoming transactions</returns>
-        public static List<Transaction> ReadProcessTransactions(string customerId)
+        public static List<Transaction> ReadProcessTransactions(string customerId, string email, string password)
         {
             const string sql = "SELECT * FROM tTransaction_Account-Account WHERE recipientID = @recipientID";
+
+            var utf16 = new UnicodeEncoding();
+
+            byte[] privateKey = Customer.GetRSAPrivateKey(email, password);
 
             var recipientIDParam = new MySqlParameter("@recipientID", MySqlDbType.VarChar, 45)
             {
@@ -120,13 +146,38 @@ namespace SORA_Class
                 Value = customerId
             };
 
+            //Encrypt the nominal with RSA.
+            //byte[] eTransactionNominal = RSA.RSADecrypt(transactionNominalBytes, recipientPublicKey);
+
             Connection connection = new Connection();
             MySqlDataReader result = MySqlHelper.ExecuteReader(connection.DbConnection, sql, recipientIDParam);
 
+            List<Transaction> transactionList = new List<Transaction>();
             while (result.Read() == true)
             {
                 Transaction transaction = new Transaction();
+                //transaction.Sender = Customer.FINDUSERFROMID(result.GetValue(0).ToString());
+                //transaction.Recepient = Customer.FINDUSERFROMID(result.GetValue(1).ToString());
+
+                byte[] eTransactionNominal = (byte[])(result["transaction_nominal"]);
+                byte[] transactionNominalBytes = RSA.RSADecrypt(eTransactionNominal, privateKey);
+                transaction.Nominal = decimal.Parse(utf16.GetString(transactionNominalBytes));
+
+                transaction.TransactionDate = DateTime.Parse(result.GetValue(3).ToString());
+
+                int completedInt = int.Parse(result.GetValue(4).ToString());
+                if (completedInt == 0)
+                {
+                    transaction.Completed = false;
+                }
+                else
+                {
+                    transaction.Completed = true;
+                }
+
+                transactionList.Add(transaction);
             }
+            return transactionList;
         }
     }
 }
