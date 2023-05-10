fa78188e60e27b53d1698b997470cf15557faa72
hidari-nut <madevis02@gmail.com>
2023-05-10 15:25:45 +0700

    Update

--- SORA_Project/SORA_Project/Properties/Resources.Designer.cs
+++ SORA_Project/SORA_Project/Properties/Resources.Designer.cs
@@ -203,6 +203,16 @@ namespace SORA_Project.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Buttons_Top_Up {
+            get {
+                object obj = ResourceManager.GetObject("Buttons Top Up", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Buttons_Transfer {
             get {
                 object obj = ResourceManager.GetObject("Buttons Transfer", resourceCulture);
